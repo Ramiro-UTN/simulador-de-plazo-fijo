3962fddc3bb46fb1a6a8541f1f510d5d9d1e624a
Ramiro Lonatti <ramirolonatti+1@gmail.com>
2022-05-08 15:59:49 -0300

    Agrego atributos requires a los inputs en el html y valido los inputs en el script

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ const cerrarBtn = document.querySelector('.cerrar');
 
 //Abre el cuadro con los datos luego de presionar el botón "calcular"
 calcularBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    datosOutput.showModal();
+
+    let nombre = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
+    let dias = document.getElementById('dias').value;
+    let monto = document.getElementById('monto').value;
+
+    //Si todos los campos  son completados de forma correcta, evito que se refresque la página y muestro los datos
+    if (nombre !== "" && apellido !== "" && dias !== "" && monto !== "" && monto >= 1000 && dias >= 30) {
+
+        e.preventDefault();
+        datosOutput.showModal();
+
+    }
+
+
+
 })
 
 cerrarBtn.addEventListener('click', () => {
