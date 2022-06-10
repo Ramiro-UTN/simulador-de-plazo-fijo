fe3b092bbe838ee9ec284144727b6ab898c0d2c2
Ramiro Lonatti <ramirolonatti+1@gmail.com>
2022-06-10 20:17:31 -0300

    Agregada función para mostrar tabla de reinversión

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let app = Vue.createApp({
             nombre: '',
             apellido: '',
             montoInicial: null,
+            montoInicialFijo: null,
             dias: null,
             reinvertir: true,
             abrirVentana: false,
             porcentaje: null,
-
-
-
-
+            periodo: 1,
+            periodosReinversion: [],
+            montoFinal: null
 
         }
     },
@@ -22,6 +22,13 @@ let app = Vue.createApp({
 
                 e.preventDefault();
                 this.ValidarPorcentajeSegunDias();
+                this.calcularMontoFinal();
+                if (this.reinvertir) {
+                    for (let i = 0; i < 4; i++) {
+                        this.reinversion();
+                    }
+
+                }
 
                 this.abrirVentana = true;
 
@@ -30,119 +37,39 @@ let app = Vue.createApp({
         },
         cerrarVentana() {
             this.abrirVentana = false;
+
+            this.periodosReinversion = [];
+            this.periodo = 1;
+            this.montoInicial = null;
+            this.montoInicialFijo = null;
+            this.dias = null;
+
         },
         ValidarPorcentajeSegunDias() {
-            console.log('dias:', this.dias + 'monto:', this.montoInicial)
+
             if (this.dias >= 30 && this.dias <= 60) this.porcentaje = 40;
             if (this.dias > 60 && this.dias <= 120) this.porcentaje = 45;
             if (this.dias > 120 && this.dias < 360) this.porcentaje = 50;
             if (this.dias >= 360) this.porcentaje = 65;
-            console.log('dias: ', this.dias + 'monto: ', this.montoInicial, 'porcentaje: ' + this.porcentaje)
-
-        }
-
-    },
-    computed: {
-
-        calcularMontoFinal() {
-
-            console.log('dias: ', this.dias + 'monto: ', this.montoInicial, 'porcentaje: ' + this.porcentaje);
+        },
+        reinversion() {
 
             let montoFinal = this.montoInicial + (this.montoInicial * ((this.dias / 360)) * (+this.porcentaje / 100));
 
-            return montoFinal.toFixed(2);
+            this.periodosReinversion.push({ nroPeriodo: this.periodo++, inicial: this.montoInicial, final: montoFinal })
+            this.montoInicial = montoFinal;
+
+
+
+        },
+        calcularMontoFinal() {
+
+            let montoFinal = this.montoInicial + (this.montoInicial * ((this.dias / 360)) * (+this.porcentaje / 100));
+            this.montoInicialFijo = this.montoInicial;
+            this.montoFinal = montoFinal.toFixed(2);
         }
 
     }
 }).mount('#app')
 
 
-/* 
-function MostrarDatos() {
-
-
-    //Muestro monto final
-
-    let outputMontoFinal = document.querySelector('.output-monto-final');
-    let porcentaje = ValidarPorcentajeSegunDias(dias);
-    let montoFinal = CalcularMontoFinal(monto, dias, porcentaje);
-    outputMontoFinal.textContent = "$ " + montoFinal;
-
-
-    //Muestro Datos de reinversion si hiciera falta
-
-    //Verifico si está seleccionada la opcion "si"
-    let reinversion = document.querySelector('#radio-opcion-si').checked;
-
-
-
-    if (reinversion) {
-
-        //Si elige reinvertir guardo los valores del monto inicial y monto final, de cada período, en un array
-        let montos = [];
-
-        //Guardo valores del primer período que habia calculado previamente
-        montos.push([monto, montoFinal])
-
-        //El bucle for llama a la funcíon CalcularMontoFinal para los tres períodos restantes, utilizando como monto incial 
-        //el monto final del período anterior, luego guarda los valores en el array "montos".
-        for (let i = 1; i < 4; i++) {
-
-            montoFinal = CalcularMontoFinal(montoFinal, dias, porcentaje);
-
-            // [montos[i - 1][1] es el lugar donde se guardó el valor del monto final del periodo anterior al que se está calculando
-            // en esta iteración del for. El monto final de la iteración anterior se guarda como monto inicial de la nueva iteración.
-            montos.push([montos[i - 1][1], montoFinal])
-        }
-
-        //Llamo función que se encarga de completar la tabla de reinversión.
-        MostrarDatosReinversion(montos);
-    } else {
-
-        // Oculto tabla si el usuario no quiere reinvertir
-        document.querySelector('.table-container').classList.add('hide');
-    }
-
-}
-
-function CalcularMontoFinal(montoInicial, dias, porcentaje) {
-    let montoFinal;
-
-    montoFinal = +montoInicial + (+montoInicial * ((+dias / 360)) * (+porcentaje / 100));
-
-    return montoFinal.toFixed(2);
-}
-
-function MostrarDatosReinversion(montos) {
-
-    //Muestra la tabla
-    document.querySelector('.table-container').classList.remove('hide');
-
-    //Selecciono cada fila de la tabla por período
-    let periodo1 = document.querySelectorAll('.periodo-uno td');
-    let periodo2 = document.querySelectorAll('.periodo-dos td');
-    let periodo3 = document.querySelectorAll('.periodo-tres td');
-    let periodo4 = document.querySelectorAll('.periodo-cuatro td');
-
-    //Completo cada celda con los datos guardados en el array que se pasó por parametro.
-    for (let i = 1; i < periodo1.length; i++) {
-        periodo1[i].textContent = "$ " + montos[0][i - 1];
-        periodo2[i].textContent = "$ " + montos[1][i - 1];
-        periodo3[i].textContent = "$ " + montos[2][i - 1];
-        periodo4[i].textContent = "$ " + montos[3][i - 1];
-    }
-
-
-}
-
-function ValidarPorcentajeSegunDias(dias) {
-    let porcentaje;
-
-    if (dias >= 30 && dias <= 60) porcentaje = 40;
-    if (dias > 60 && dias <= 120) porcentaje = 45;
-    if (dias > 120 && dias < 360) porcentaje = 50;
-    if (dias >= 360) porcentaje = 65;
-
-    return porcentaje;
-
-} */
